4aa2c6b029f6ca5552c55a54153bdf7a20b87e87
Neno Albert <Demonneno@gmail.com>
2025-04-04 15:32:35 -0500

    updates

--- init.lua
+++ init.lua
@@ -642,11 +642,13 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+        html = {},
+        jinja_lsp = {},
         -- ruff-lsp = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
+        -- Some languages (like typescript) have entire language plugins that can be useful
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
